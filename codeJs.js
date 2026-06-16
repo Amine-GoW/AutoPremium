8b15d47cdda0d1b03ee07ea44932107c6b03ea38
Amine <bouayadi49@gmail.com>
2026-06-16 14:36:39 +0200

    Toques finales

--- codeJs.js
+++ codeJs.js
@@ -6,10 +6,29 @@ function Mover(){
 botonVerCoches.addEventListener("click",Mover);
 
 const btnMasInfo = document.querySelectorAll(".masInfo");
-
+let cont=0;
 btnMasInfo.forEach(btn => {
     btn.addEventListener("click", function () {
         const tarjeta = this.closest(".contenedorGira");
+        if(!tarjeta.classList.contains("giro")){
+            cont++;
+            document.getElementById("vistos").innerHTML="Coches vistos: "+cont;
+        }
         tarjeta.classList.toggle("giro");
     });
 });
+
+const todasTarjetas= document.querySelectorAll(".contenedorGira");
+document.addEventListener("click",function(evento){
+    todasTarjetas.forEach(function(tarjeta){
+        if (tarjeta.classList.contains("giro")) {
+            if(!tarjeta.contains(evento.target)){
+                tarjeta.classList.remove("giro");
+            }
+        }
+    });
+});
+const btnEnviar=document.getElementById("enviar");
+btnEnviar.addEventListener("click",function(){
+    alert("Tus informacion han sido enviada, Gracias");
+})
